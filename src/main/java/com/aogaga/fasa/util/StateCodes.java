eb8b3ddf4be62b3b29bb67678b1d439b762c3a56
Ogheneogaga Agi <aogaga@yahoo.com>
2025-12-16 21:03:41 -0600

    updated states map

--- src/main/java/com/aogaga/fasa/util/StateCodes.java
+++ src/main/java/com/aogaga/fasa/util/StateCodes.java
@@ -3,5 +3,11 @@ package com.aogaga.fasa.util;
 import java.util.Set;
 
 public class StateCodes {
-    public static final Set<String> ALL = Set.of("AL","AK","AZ","AR","CA","NY","TX", /*...*/ "WI","WY");
+    public static final Set<String> ALL = Set.of(
+            "AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA",
+            "HI","ID","IL","IN","IA","KS","KY","LA","ME","MD",
+            "MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ",
+            "NM","NY","NC","ND","OH","OK","OR","PA","RI","SC",
+            "SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"
+    );
 }
